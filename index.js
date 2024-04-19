257c87abbc9a72ed2bc60cde9c8d54bc97dc8db5
unknown <https://github.com/wardamalik545>
2024-04-19 02:43:49 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one of the operators to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    },
+]);
+//condition statement
+if (answer.operator === "Addition") {
+    let result = answer.firstNumber + answer.secondNumber;
+    console.log("Your answer is: " + result);
+}
+else if (answer.operator === "Subtraction") {
+    let result = answer.firstNumber - answer.secondNumber;
+    console.log("Your answer is: " + result);
+}
+else if (answer.operator === "Multiplication") {
+    let result = answer.firstNumber * answer.secondNumber;
+    console.log("Your answer is: " + result);
+}
+else if (answer.operator === "Division") {
+    let result = answer.firstNumber / answer.secondNumber;
+    console.log("Your answer is: " + result);
+}
+else {
+    console.log("please select valid operator");
+}
